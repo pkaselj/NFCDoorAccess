741849128fba25b9cb63d7a1e4a6c6dc5c20ca69
pkaselj <petar.kaselj.00@gmail.com>
2021-06-01 16:49:32 +0200

    Added copyright

--- ErrorCodes/include/errorCodes.h
+++ ErrorCodes/include/errorCodes.h
@@ -1,3 +1,27 @@
+/*
+*	 Copyright (C) Petar Kaselj 2021
+*
+*	 This file is part of NFCDoorAccess.
+*
+*	 NFCDoorAccess is written by Petar Kaselj as an employee of
+*	 Emovis tehnologije d.o.o. which allowed its release under
+*	 this license.
+*
+*    NFCDoorAccess is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    NFCDoorAccess is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with NFCDoorAccess.  If not, see <https://www.gnu.org/licenses/>.
+*
+*/
+
 //test line
 #ifndef ERROR_CODES_H
 #define ERROR_CODES_H
